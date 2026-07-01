b20e1e7ad3d9423c53e78505ff28c5c6d30beac6
Kelia Simbi <simbikelia@gmail.com>
2026-07-01 20:49:46 +0200

    implement login

--- backend/src/auth/auth.controller.ts
+++ backend/src/auth/auth.controller.ts
@@ -1,6 +1,7 @@
-import {Body, Controller, Post} from '@nestjs/common';
+import {Body, Controller, HttpCode, HttpStatus, Post} from '@nestjs/common';
 import {AuthService} from './auth.service';
 import {RegisterDto} from './dto/register.dto';
+import {LoginDto} from './dto/login.dto';
 
 @Controller('auth')
 export class AuthController{
@@ -9,5 +10,10 @@ export class AuthController{
     @Post('register')
     register(@Body() registerDto: RegisterDto){
         return this.authservice.register(registerDto);
+    }
+    @HttpCode(HttpStatus.OK)
+    @Post('login')
+    async login(@Body() loginDto: LoginDto){
+        return this.authservice.login(loginDto);
     }
 }
--- backend/src/auth/auth.service.ts
+++ backend/src/auth/auth.service.ts
@@ -1,13 +1,15 @@
 import {Injectable} from '@nestjs/common';
 import {PrismaService} from '../prisma/prisma.service';
-import { BadRequestException } from '@nestjs/common';
+import { BadRequestException, UnauthorizedException } from '@nestjs/common';
 import * as bcrypt from 'bcrypt';
 import { RegisterDto} from './dto/register.dto'
+import { JwtService } from '@nestjs/jwt';
+import { LoginDto } from './dto/login.dto';
 
 @Injectable()
 
 export class AuthService{
-    constructor(private readonly prisma: PrismaService){}
+    constructor(private readonly prisma: PrismaService, private readonly jwtService: JwtService){}
     async register(registerDto: RegisterDto){
         const {username, email, password} = registerDto;
         const existingUser = await this.prisma.user.findUnique({
@@ -32,5 +34,31 @@ export class AuthService{
         const { password:_, ...safeUser} = user;
         return safeUser;
 
+    }
+    async login(loginDto: LoginDto){
+        const {email, password} = loginDto;
+        const user = await this.prisma.user.findUnique({
+            where:{email},
+        });
+        if (!user){
+            throw new UnauthorizedException('Invalid credentials');
+
+        }
+        const isPasswordValid = await bcrypt.compare(password, user.password);
+        if(!isPasswordValid){
+            throw new UnauthorizedException('Invalid credentials');
+        }
+
+        const payload = {sub: user.id, email: user.email, username:user.username};
+
+        return{
+            access_token: await
+            this.jwtService.signAsync(payload),
+            user:{
+                id: user.id,
+                email: user.email,
+                username:user.username
+            }
+        };
     }
 }
